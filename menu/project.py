84f6456b05bf73efbbb83536275cc7215311aeb3
Tayo Madein <tuby1805@gmail.com>
2018-01-17 05:56:49 +0100

    Load menu by restaurant

--- menu/project.py
+++ menu/project.py
@@ -1,12 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
 
+## import for CRUD operations
+from database_setup import Restaurant, Base, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello Flask World!"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += "</br>"
+        output += i.price
+        output += "</br>"
+        output += i.description
+        output += "</br>"
+        output += "</br>"
+    return output
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
-    
