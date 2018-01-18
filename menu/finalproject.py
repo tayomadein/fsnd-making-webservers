6fdaba2b2460470a6a3103f706b9567891496bde
Tayo Madein <tuby1805@gmail.com>
2018-01-18 09:39:03 +0100

    connect to db

--- menu/finalproject.py
+++ menu/finalproject.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
+## import for CRUD operations
+from database_setup import Restaurant, Base, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
+
+#Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants')
