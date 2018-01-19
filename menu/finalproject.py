ac5becfaefce03881ca3a42f8068d940e4f878df
Tayo Madein <tuby1805@gmail.com>
2018-01-19 07:34:00 +0100

    add secret_key

--- menu/finalproject.py
+++ menu/finalproject.py
@@ -128,5 +128,6 @@ def menuItemJSON(restaurant_id, menu_id):
     return jsonify(MenuItem=menuItem.serialize)
 
 if __name__ == '__main__':
+    app.secret_key = 'my_super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
